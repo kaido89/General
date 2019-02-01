d896864504d72325d35c8624d925f05efb74f965
mzhu1 <mzhu1@illumina.com>
2019-02-01 16:50:42 +0000

    added ldap api

---
+++ tools_API/README.md
@@ -0,0 +1,4 @@
+Here is a list of tools using python3 API:
+- Security Center
+- Splunk
+- LDAP
---
+++ tools_API/ldap/ldap_api.py
@@ -0,0 +1,21 @@
+from ldap3 import Server, Connection, ALL, ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES
+import os
+import csv
+import datetime
+
+
+def main():
+    ldap_server = input('LDAP url (eg. example.com): ')
+    server = Server(ldap_server, get_info=ALL)
+    ldap_user = input('LDAP USER (eg. KAIDO89): ')
+    ldap_pass = input('LDAP PASSWORD (eg. KAIDO89_PASSWORD): ')
+    forest = input('LDAP FOREST (eg. COMPANY_NAME): ')
+    Connection(server, user=forest+"\\"+ldap_user, password=ldap_pass, auto_bind=True)
+    search_user = input('SEARCH USER in LDAP (eg. KAIDO89_FRIEND): ')
+    search_user_result = conn.search('dc='+str(forest).lower()+',dc=com',
+                                     '(&(objectclass=person)(mailNickname=' + search_user + '))',
+                                     attributes=[ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES])
+    print(search_user_result)
+
+
+main()
